7ee813dbe4ed31fb27bb6ed20daea33d7fb73bf0
shazy89 <erdo.shazy123@gmail.com>
2021-05-24 19:24:35 +0200

    display the boxes

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,16 +1,28 @@
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
+import DashboardBox from "./DashboardBox";
+import { Container, Row, Col } from "react-bootstrap";
 import axios from "axios";
 
 import PropTypes from "prop-types";
 
 const Dashboard = ({ isAuthenticated }) => {
   return (
-    <>
+    <Container>
+      <Row>
+        <Col>
+          <DashboardBox />
+        </Col>
+        <Col>
+          <DashboardBox />
+        </Col>
+        <Col>
+          <DashboardBox />
+        </Col>
+      </Row>
       <div></div>
       <div></div>
-      <div></div>
-    </>
+    </Container>
   );
 };
 
--- src/components/dashboard/DashboardBox.js
+++ src/components/dashboard/DashboardBox.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const DashboardBox = () => {
-  return <div></div>;
+  return <div className="dashboard__box"></div>;
 };
 
 export default DashboardBox;
